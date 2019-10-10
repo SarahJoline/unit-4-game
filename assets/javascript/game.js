a7397c4602c90e1a81c78d5b3f1baa03a40a29ac
Sarah Naas <sjnaas@gmail.com>
2019-10-10 16:01:45 -0700

    put my variables in an array, added a reset sheet, and a conditional

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -2,24 +2,87 @@
 var randomNumber = Math.floor((Math.random() * 120) + 19);
 console.log(randomNumber)
 var score = 0;
-var emeraldValue = Math.floor((Math.random() * 12) + 1);
-var diamondValue = Math.floor((Math.random() * 12) + 1);
-var sapphireValue = Math.floor((Math.random() * 12) + 1);
-var rubyValue = Math.floor((Math.random() * 12) + 1);
-
-//4 crystals that are actually buttons. 
+var wins = 0;
+var losses = 0;
 
 //Each crystal his assigned a random hidden value between 1-12
+// var emeraldValue = Math.floor((Math.random() * 12) + 1);
+// var diamondValue = Math.floor((Math.random() * 12) + 1);
+// var sapphireValue = Math.floor((Math.random() * 12) + 1);
+// var rubyValue = Math.floor((Math.random() * 12) + 1);
 
-$('#emerald').on('click', function () {
-    var valueToAdd = emeraldValue;
+// //4 crystals that are actually buttons. 
+
+// // when a crystal is clicked, the value is added to the score.
+
+// function filler() {
+
+//     if (score === randomNumber) {
+//         wins++;
+//         alert('you win');
+//     } else if (score > randomNumber) {
+//         losses++;
+//         alert('you lose');
+//     };
+
+//     $('#emerald').on('click', function () {
+//         var valueToAdd = emeraldValue;
+//         score += parseInt(valueToAdd);
+//         console.log(score);
+//     });
+//     $('#diamond').on('click', function () {
+//         var valueToAdd = diamondValue;
+//         score += parseInt(valueToAdd);
+//         console.log(score);
+//     });
+//     $('#sapphire').on('click', function () {
+//         var valueToAdd = sapphireValue;
+//         score += parseInt(valueToAdd);
+//         console.log(score);
+//     });
+//     $('#ruby').on('click', function () {
+//         var valueToAdd = rubyValue;
+//         score += parseInt(valueToAdd);
+//         console.log(score);
+//     });
+// }
+
+// if the total score is equal to the random number the payer wins
+// Made an array that holds each variable that is set to a random number between 1 and 12
+new Array(emerald, diamond, sapphire, ruby);
+
+var gems = new Array;
+
+var emerald = Math.floor((Math.random() * 12) + 1);
+var diamond = Math.floor((Math.random() * 12) + 1);
+var sapphire = Math.floor((Math.random() * 12) + 1);
+var ruby = Math.floor((Math.random() * 12) + 1);
+
+gems.push(emerald);
+gems.push(diamond);
+gems.push(sapphire);
+gems.push(ruby);
+
+console.log(gems);
+
+gems.forEach(element => {
+    $('body').append(`<button data='${element}'>`);
+});
+
+$('button').on('click', function () {
+    var valueToAdd = $(this).attr('data');
     score += parseInt(valueToAdd);
     console.log(score);
-})
-// when a crystal is clicked, the value is added to the score.
 
-//if the total score is equal to the random number the payer wins
+    if (score === randomNumber) {
+        wins++;
+        alert('you win');
+    } else if (score > randomNumber) {
+        losses++;
+        alert('you lose');
+    };
+});
 
-//if the score is greater than the random number the player loses. 
+//if the score is greater than the random number the player loses.
 
 // game restarts
